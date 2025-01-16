f8f9a101d745098ef94458ecd2d1ed5c1880e11e
Ed <edouard.duron@lapiscine.pro>
2025-01-16 13:49:14 +0100

    many modification -feat Tatania added CSS, JS, login, changed Entities

--- src/Controller/public/HomeController.php
+++ src/Controller/public/HomeController.php
@@ -2,18 +2,7 @@
 
 namespace App\Controller\public;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
-
-class HomeController extends AbstractController
+class HomeController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
-    }
+
 }
